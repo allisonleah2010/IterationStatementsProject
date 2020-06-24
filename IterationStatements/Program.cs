43dce23cee06a7e322b7504ef62b6f1fff356b89
Allison Cole <allisonleahcole@gmail.com>
2020-06-24 15:53:05 -0400

    loops

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,43 +11,56 @@ namespace IterationStatements
 
             //Create a List called "numbers" - DONE
             var numbers = new List<int>();
-            
-            //Create a variable of type int and initialize the variable with a value of 0
 
+            //Create a variable of type int and initialize the variable with a value of 0 - DONE
 
+            var myNumber = 0;
 
-            // Create a do-while loop
+            // Create a do-while loop - DONE
+            do
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
-            }
-            // While your variable is less than 100
+                // Increment your variable by 1 - DONE
+                myNumber++;
+                // Then add your variable to "numbers" - DONE
+                numbers.Add(myNumber);
+            } while (myNumber < 100);
 
+            // While your variable is less than 100
 
 
             // Create a while loop
             // While your variable is less than 200
+
+            while (myNumber < 200)
             {
                 // Increment your variable by 1
+                myNumber++;
                 // Then add your variable to "numbers"
+                numbers.Add(myNumber);
             }
+
+            Console.WriteLine($"{myNumber} is less than 200!");
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
-            // Write your variable to the console
 
-            Console.WriteLine("");
+            foreach (var item in numbers)
+            {
+                Console.WriteLine($"{item}");
+            }
+
             Console.WriteLine("Decrease:");
 
-            // Create a for loop
             // in your initializer set the value of i to 199
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
-                // Write to the console "numbers" at index i
+                Console.WriteLine($"{numbers[i]}");
             }
+            // Write to the console "numbers" at index i
         }
     }
 }
